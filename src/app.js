9830cd3411558b6c518b998f8c0c0721b33f0597
Salmah <salmahabdirahman@gmail.com>
2023-03-05 03:38:01 +0400

    Added weather icons

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
-function formatDate(date) {
-  let currentHours = date.getHours();
-  if (currentHours < 10) {
-    currentHours = `0${currentHours}`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
 
-  let currentMinutes = date.getMinutes();
-  if (currentMinutes < 10) {
-    currentMinutes = `0${currentMinutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
 
-  let day = date.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -19,60 +20,117 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  let currentDay = days[day];
+  let day = days[date.getDay()];
 
-  return `${currentDay}, ${currentHours}:${currentMinutes}`;
+  return `${day}, ${hours}:${minutes}`;
 }
 
-let date = document.querySelector("#date");
-let currentTime = new Date();
-
-date.innerHTML = formatDate(currentTime);
-
-function showWeatherCondition(response) {
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].description;
-
-  document.querySelector("#city").innerHTML = `${response.data.name}`;
-
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-
-  document.querySelector("#pressure").innerHTML = response.data.main.pressure;
-
-  document.querySelector("#visibility").innerHTML = response.data.visibility;
-
-  let iconElement = document.querySelector("#main-icon");
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
-
-  getForecast(response.data.coord);
-
-  console.log(response.data);
+function searchCity(city) {
+  let apiKey = "b753892e4baf541ac20f62af54c786fa";
+  let units = "metric";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showWeatherCondition);
 }
 
-function getForecast(coordinates) {
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  searchCity(city);
+}
+
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "metric";
+  let apiKey = "b753892e4baf541ac20f62af54c786fa";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showWeatherCondition);
+}
+
+function getCurrentPosition() {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function formatHour(timestamp) {
+  let date = new Date(timestamp * 1000);
+
+  let hours = date.getHours();
+
+  if (hours < 10) {
+    hours = `${hours} AM`;
+  } else {
+    hours = `${hours} PM`;
+  }
+
+  return hours;
+}
+
+function getHourlyForecast(coordinates) {
   let apiEndpoint = `https://api.openweathermap.org/data/2.5/onecall`;
   let apiKey = "f3887e262c88d1158f7e2ef4998e234c";
   let units = "metric";
   let apiUrl = `${apiEndpoint}?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(displayForecast);
+  axios.get(apiUrl).then(displayHourlyForecast);
 }
 
-function displayForecast(response) {
+function displayHourlyForecast(response) {
+  let forecast = response.data.hourly;
+
+  let forecastElement = document.querySelector("#hourly-forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+
+        <div class="col card">
+              <p class="mb-1">${formatHour(forecastDay.dt)}</p>
+             <img id="icon"
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
+                  alt=""
+
+                />
+              <p id="hourly-temperature">${Math.round(forecastDay.temp)}º</p>
+            </div>
+
+
+            
+            `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function getDailyForecast(coordinates) {
+  let apiEndpoint = `https://api.openweathermap.org/data/2.5/onecall`;
+  let apiKey = "f3887e262c88d1158f7e2ef4998e234c";
+  let units = "metric";
+  let apiUrl = `${apiEndpoint}?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayDailyForecast);
+}
+
+function displayDailyForecast(response) {
   let forecast = response.data.daily;
 
-  let forecastElement = document.querySelector("#week-weather-forecast");
+  let forecastElement = document.querySelector("#daily-forecast");
 
   let forecastHTML = `<div class="row">`;
 
@@ -107,18 +165,40 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
-function searchCity(city) {
-  let apiKey = "b753892e4baf541ac20f62af54c786fa";
-  let units = "metric";
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showWeatherCondition);
-}
+function showWeatherCondition(response) {
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
 
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#search-input").value;
-  searchCity(city);
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+
+  document.querySelector("#city").innerHTML = `${response.data.name}`;
+
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+
+  document.querySelector("#pressure").innerHTML = response.data.main.pressure;
+
+  document.querySelector("#visibility").innerHTML = response.data.visibility;
+
+  let iconElement = document.querySelector("#main-icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getDailyForecast(response.data.coord);
+
+  getHourlyForecast(response.data.coord);
 }
 
 let searchForm = document.querySelector("#search-form");
@@ -126,28 +206,5 @@ searchForm.addEventListener("submit", handleSubmit);
 
 searchCity("New York");
 
-function showPosition(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let units = "metric";
-  let apiKey = "b753892e4baf541ac20f62af54c786fa";
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-
-  axios.get(apiUrl).then(showWeatherCondition);
-}
-
-function getCurrentPosition() {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(showPosition);
-}
-
 let currentLocationButton = document.querySelector("#current-location");
 currentLocationButton.addEventListener("click", getCurrentPosition);
-
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  return days[day];
-}
